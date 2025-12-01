486e49d43db5f1e08835f62386640f3ff1abbdff
jaylen <jsan6767@students.vsu.edu>
2025-11-30 20:52:42 -0500

    Nav bar and everything else

--- app/src/main/java/com/codepath/lab6/CampgroundFragment.kt
+++ app/src/main/java/com/codepath/lab6/CampgroundFragment.kt
@@ -10,20 +10,22 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
-import kotlin.let
 
 private const val TAG = "CampgroundFragment"
-private const val API_KEY = BuildConfig.API_KEY
-private const val CAMPGROUND_URL =
+private val API_KEY = BuildConfig.API_KEY
+private val CAMPGROUND_URL =
     "https://developer.nps.gov/api/v1/campgrounds?api_key=${API_KEY}"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CampgroundFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+// JSON parser
+fun makeJsonParser() = Json {
+    isLenient = true
+    ignoreUnknownKeys = true
+    useAlternativeNames = false
+}
+
 class CampgroundFragment : Fragment() {
 
     private val campgrounds = mutableListOf<Campground>()
@@ -32,39 +34,32 @@ class CampgroundFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Change this statement to store the view in a variable instead of a return statement
+
         val view = inflater.inflate(R.layout.fragment_campground, container, false)
 
-        // Add these configurations for the recyclerView and to configure the adapter
-        val layoutManager = LinearLayoutManager(context)
+        // RecyclerView setup
         campgroundsRecyclerView = view.findViewById(R.id.campground_recycler_view)
-        campgroundsRecyclerView.layoutManager = layoutManager
+        campgroundsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         campgroundsRecyclerView.setHasFixedSize(true)
-        campgroundAdapter = CampgroundAdapter(view.context, campgrounds)
+
+        campgroundAdapter = CampgroundAdapter(requireContext(), campgrounds)
         campgroundsRecyclerView.adapter = campgroundAdapter
 
         fetchCampgrounds()
 
-        // Update the return statement to return the inflated view from above
         return view
-    }
-
-    companion object {
-        fun newInstance(): CampgroundFragment {
-            return CampgroundFragment()
-        }
     }
 
     private fun fetchCampgrounds() {
         val client = AsyncHttpClient()
         client.get(CAMPGROUND_URL, object : JsonHttpResponseHandler() {
+
             override fun onFailure(
                 statusCode: Int,
                 headers: Headers?,
@@ -74,19 +69,25 @@ class CampgroundFragment : Fragment() {
                 Log.e(TAG, "Failed to fetch campgrounds: $statusCode")
             }
 
-            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
-                Log.i(TAG, "Successfully fetched campgrounds: $json")
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Headers,
+                json: JSON
+            ) {
+                Log.i(TAG, "Successfully fetched campgrounds")
+
                 try {
-                    val parsedJson = createJson().decodeFromString(
-                        CampgroundResponse.serializer(),
+                    val parsed = makeJsonParser().decodeFromString<CampgroundResponse>(
                         json.jsonObject.toString()
                     )
-                    parsedJson.data?.let { list ->
+
+                    parsed.data?.let { list ->
                         campgrounds.addAll(list)
                         campgroundAdapter.notifyDataSetChanged()
                     }
+
                 } catch (e: JSONException) {
-                    Log.e(TAG, "Exception: $e")
+                    Log.e(TAG, "JSON exception: $e")
                 }
             }
         })
